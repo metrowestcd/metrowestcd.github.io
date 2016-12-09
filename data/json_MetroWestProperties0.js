9e562c32f9b3980dc32ca8570542d3981ca141f0
metrowestcd <holly@metrowestcd.org>
2016-12-09 17:37:02 -0500

    edit2

--- data/json_MetroWestProperties0.js
+++ data/json_MetroWestProperties0.js
@@ -1,4 +1,4 @@
-var geojson_MetroWestProperties = {
+var json_MetroWestProperties0={
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
